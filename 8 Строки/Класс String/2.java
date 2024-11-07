821e29e6d5dcbb0abe746ef9ceef02932663e3ba
Ivan4ikSh <shupaevi000@gmail.com>
2024-11-07 05:01:51 +0300

    1

---
+++ 8 Строки/Класс String/2.java
@@ -0,0 +1,16 @@
+import java.util.StringJoiner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создание объекта StringJoiner с разделителем ", "
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+
+        // Добавляем строки в StringJoiner
+        joiner.add("Apple");
+        joiner.add("Banana");
+        joiner.add("Cherry");
+
+        // Выводим результат
+        System.out.println(joiner.toString()); // Вывод: [Apple, Banana, Cherry]
+    }
+}
