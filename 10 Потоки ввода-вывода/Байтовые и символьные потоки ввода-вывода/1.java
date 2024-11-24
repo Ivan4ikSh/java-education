2fcf0c30dbc6c515e8f6ec7ef3322e3ca1f6f521
Ivan4ikSh <shupaevi000@gmail.com>
2024-11-24 22:18:44 +0300

    10.0

---
+++ 10 Потоки ввода-вывода/Байтовые и символьные потоки ввода-вывода/1.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "example.txt"; // Путь к файлу, который будет прочитан
+
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+            int data;
+            // Чтение байтов из файла до конца
+            while ((data = inputStream.read()) != -1) {
+                // Преобразование байта в символ и вывод на экран
+                System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+    }
+}
---
+++ 10 Потоки ввода-вывода/Байтовые и символьные потоки ввода-вывода/2.java
@@ -0,0 +1,18 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "output.txt"; // Путь к файлу, в который будет записан текст
+
+        try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+            String text = "Hello, World!";
+            // Запись каждого символа в файл
+            for (char c : text.toCharArray()) {
+                outputStream.write(c); // Запись символа в виде байта
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
+}
